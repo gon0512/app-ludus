245298e278f55a61f1fdc5c5376002969f4218d4
gon0512 <layonfigueiroa@gmail.com>
2022-02-27 22:46:33 -0300

    criacao do server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3003
+
+app.get('/', (req, res) => {
+    return res.send('API app-ludus')
+})
+
+app.listen(port)
